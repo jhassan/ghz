98c1c937bd520c64cbd8ee557f6d18ac17a50d44
jhassan <jawadjee0519@gmail.com>
2025-09-20 15:15:46 +0500

    update code

--- app/Console/Commands/SendCryptoSymbolsEmail.php
+++ app/Console/Commands/SendCryptoSymbolsEmail.php
@@ -2,11 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Mail\CryptoSymbolsMail;
 use Illuminate\Console\Command;
-
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\CryptoSymbolsMail;
 
 class SendCryptoSymbolsEmail extends Command
 {
@@ -30,22 +29,23 @@ class SendCryptoSymbolsEmail extends Command
     public function handle()
     {
         // $symbols = ["BTC", "ICP", "DOT", "NEAR", "ORDI", "INJ", "NEIRO", "SOL", "OP", "XRP", "ADA"];
-        $symbols = ["BTCUSDT", "ICPUSDT", "DOTUSDT", "NEARUSDT", "ORDIUSDT", "INJUSDT", "NEIROUSDT", "SOLUSDT", "OPUSDT", "XRPUSDT", "ADAUSDT"];
+        $symbols = ['BTCUSDT', 'ICPUSDT', 'DOTUSDT', 'NEARUSDT', 'ORDIUSDT', 'INJUSDT', 'NEIROUSDT', 'SOLUSDT', 'OPUSDT', 'XRPUSDT', 'ADAUSDT'];
         // get latest record per symbol
         $cryptoData = DB::table('candles as cp')
             ->join(DB::raw("(SELECT symbol, MAX(created_at) as last_created
                              FROM candles
-                             WHERE symbol IN ('" . implode("','", $symbols) . "')
+                             WHERE symbol IN ('".implode("','", $symbols)."')
                              GROUP BY symbol) latest"),
                 function ($join) {
                     $join->on('cp.symbol', '=', 'latest.symbol')
-                         ->on('cp.created_at', '=', 'latest.last_created');
+                        ->on('cp.created_at', '=', 'latest.last_created');
                 })
             ->get();
 
         // send email
-        $sendEmail = Mail::to('jawadjee0519@gmail.com')->send(new CryptoSymbolsMail($cryptoData));
-                // dd($sendEmail);
+        // $sendEmail = Mail::to('jawadjee0519@gmail.com')->send(new CryptoSymbolsMail($cryptoData));
+        $sendEmail = Mail::to('scalpinguniverseg@gmail.com')->send(new CryptoSymbolsMail($cryptoData));
+        // dd($sendEmail);
         $this->info('Crypto symbols email sent successfully!');
     }
 }
