ea9d02fc8eb7d53b7aeb6db4b71a4206981f2e86
jhassan <jawadjee0519@gmail.com>
2025-09-30 19:13:35 +0500

    30sep2025 - update cron job email

--- app/Console/Commands/SendCryptoSymbolsEmail.php
+++ app/Console/Commands/SendCryptoSymbolsEmail.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Mail\CryptoSymbolsMail;
+use App\Models\Coins;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
@@ -28,24 +30,52 @@ class SendCryptoSymbolsEmail extends Command
      */
     public function handle()
     {
+        sleep(30);
         // $symbols = ["BTC", "ICP", "DOT", "NEAR", "ORDI", "INJ", "NEIRO", "SOL", "OP", "XRP", "ADA"];
-        $symbols = ['BTCUSDT', 'ICPUSDT', 'DOTUSDT', 'NEARUSDT', 'ORDIUSDT', 'INJUSDT', 'NEIROUSDT', 'SOLUSDT', 'OPUSDT', 'XRPUSDT', 'ADAUSDT'];
+        // $symbols = ['BTCUSDT', 'ICPUSDT', 'DOTUSDT', 'NEARUSDT', 'ORDIUSDT', 'INJUSDT', 'NEIROUSDT', 'SOLUSDT', 'OPUSDT', 'XRPUSDT', 'ADAUSDT'];
+        $record = Coins::find(1); // or ->first()
+        // remove double quotes first
+        $clean = str_replace('"', '', $record->coins);
+
+        // now split by comma
+        $symbols = array_map('trim', explode(',', $clean));
         // get latest record per symbol
+        // $cryptoData = DB::table('candles as cp')
+        //     ->join(DB::raw("(SELECT symbol, MAX(created_at) as last_created
+        //                      FROM candles
+        //                      WHERE symbol IN ('".implode("','", $symbols)."')
+        //                      GROUP BY symbol) latest"),
+        //         function ($join) {
+        //             $join->on('cp.symbol', '=', 'latest.symbol')
+        //                 ->on('cp.created_at', '=', 'latest.last_created');
+        //         })
+        //     ->get();
+
+        $now = Carbon::now()->second(0);   // current time with seconds = 0
+        $startTime = $now->copy();         // e.g. 07:00:00
+        $endTime = $now->copy()->addSeconds(30); // e.g. 07:00:30
+
         $cryptoData = DB::table('candles as cp')
             ->join(DB::raw("(SELECT symbol, MAX(created_at) as last_created
-                             FROM candles
-                             WHERE symbol IN ('".implode("','", $symbols)."')
-                             GROUP BY symbol) latest"),
+                            FROM candles
+                            WHERE symbol IN ('".implode("','", $symbols)."')
+                            GROUP BY symbol) latest"),
                 function ($join) {
                     $join->on('cp.symbol', '=', 'latest.symbol')
                         ->on('cp.created_at', '=', 'latest.last_created');
                 })
+            ->whereBetween('cp.created_at', [$startTime, $endTime])
             ->get();
 
         // send email
-        // $sendEmail = Mail::to('jawadjee0519@gmail.com')->send(new CryptoSymbolsMail($cryptoData));
-        $sendEmail = Mail::to('scalpinguniverseg@gmail.com')->send(new CryptoSymbolsMail($cryptoData));
-        // dd($sendEmail);
-        $this->info('Crypto symbols email sent successfully!');
+        if (count($cryptoData) > 0) {
+            $sendEmail = Mail::to('jawadjee0519@gmail.com')->send(new CryptoSymbolsMail($cryptoData));
+            $sendEmail = Mail::to('scalpinguniverseg@gmail.com')->send(new CryptoSymbolsMail($cryptoData));
+            // dd($sendEmail);
+            $this->info('Crypto symbols email sent successfully!');
+        } else {
+            $this->info('Crypto symbols not found!');
+        }
+
     }
 }
